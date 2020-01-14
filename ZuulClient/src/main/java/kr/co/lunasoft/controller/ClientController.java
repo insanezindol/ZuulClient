06398d4e9dc04ffda292a6a8ab8e174c4c2d88f7
딘 <jhlee@lunasoft.co.kr>
2020-01-14 16:29:02 +0900

    add test call

--- ZuulClient/src/main/java/kr/co/lunasoft/controller/ClientController.java
+++ ZuulClient/src/main/java/kr/co/lunasoft/controller/ClientController.java
@@ -2,6 +2,7 @@ package kr.co.lunasoft.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,22 @@ public class ClientController {
 		obj.put("code", "100200");
 		obj.put("msg", "success");
 		obj.put("data", "this is zuul client. 67890");
+		return obj;
+	}
+	
+	@GetMapping(value = "/long-time")
+	public Map<String, Object> longTime() {
+		int rand = new Random().nextInt(10) + 10;
+		try {
+			Thread.sleep(rand * 1000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		Map<String, Object> obj = new HashMap<String, Object>();
+		obj.put("code", "100200");
+		obj.put("msg", "success");
+		obj.put("data", "this is zuul client. 12345");
 		return obj;
 	}
 
